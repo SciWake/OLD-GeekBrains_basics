d3a0190bd239bf89693b9d36459e6e09b8a34d0c
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-07-19 08:34:38 +0700

    [Алгоритмы] Домашняя работа № 3

--- Algorithms-in-python/3. Arrays/Task/Task_6.py
+++ Algorithms-in-python/3. Arrays/Task/Task_6.py
@@ -1,24 +1,25 @@
 # 6. В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами.
 # Сами минимальный и максимальный элементы в сумму не включать.
+import random as rd
 
-array = [1, 2, 3, 40, 2, -5, 1, 10]
+array = [rd.randint(-10, 10) for _ in range(10)]
+print(array)
 
 index_min = index_max = 0
 
 for i, num in enumerate(array):
-    if array[index_max] >= num:
-        index_max = i
-    if array[index_min] <= num:
+    if array[index_min] > num:
         index_min = i
+    if array[index_max] < num:
+        index_max = i
 
 sum = 0
 
 if len(array_1 := array[index_min + 1: index_max]):
     for i in array_1:
         sum += i
-elif len(array := array[index_max + 1: index_min]):
-    for i in array:
+elif len(array_2 := array[index_max: index_min]):
+    for i in array_2:
         sum += i
-
 
 print(f'Сумма между {array[index_min]} и {array[index_max]} = {sum}')
