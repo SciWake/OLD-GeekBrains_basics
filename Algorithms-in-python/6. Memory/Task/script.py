93ddcb6c22f857dc19f0614cb9928772a87eeb8b
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-01 13:08:39 +0700

    Скрипт для анализа памяти

---
+++ Algorithms-in-python/6. Memory/Task/script.py
@@ -0,0 +1,23 @@
+import sys
+
+def show_size(x, level=0):
+    print('\t' * level, f'type = {x.__class__}, size = {sys.getsizeof(x)}, object = {x}')
+
+    if hasattr(x, '__iter__'):
+        if hasattr(x, 'items'):
+            for xx in x.items():
+                show_size(xx, level + 1)
+        elif not isinstance(x, str):
+            for xx in x:
+                show_size(xx, level + 1)
+
+
+def memory_size(*args):
+    sum = 0
+    show_size(args)
+    for i in args:
+        sum += sys.getsizeof(i)
+    print(sum)
+
+
+
