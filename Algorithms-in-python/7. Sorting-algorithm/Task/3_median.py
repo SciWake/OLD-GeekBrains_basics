4eb047c72cea6ea04f099f9381e4bc8ee8dfa22f
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-06 11:52:30 +0700

    Медиана

---
+++ Algorithms-in-python/7. Sorting-algorithm/Task/3_median.py
@@ -0,0 +1,36 @@
+import random
+
+size = 100
+array = [i for i in range(0, 20)]
+random.shuffle(array)
+print(array)
+
+
+def quickselect_median(l, pivot_fn=random.choice):
+    if len(l) % 2 == 1:
+        return quickselect(l, len(l) / 2, pivot_fn)
+    else:
+        return 0.5 * (quickselect(l, len(l) / 2 - 1, pivot_fn) +
+                      quickselect(l, len(l) / 2, pivot_fn))
+
+
+def quickselect(l, k, pivot_fn):
+    if len(l) == 1:
+        assert k == 0
+        return l[0]
+
+    pivot = pivot_fn(l)
+
+    lows = [el for el in l if el < pivot]
+    highs = [el for el in l if el > pivot]
+    pivots = [el for el in l if el == pivot]
+
+    if k < len(lows):
+        return quickselect(lows, k, pivot_fn)
+    elif k < len(lows) + len(pivots):
+        return pivots[0]
+    else:
+        return quickselect(highs, k - len(lows) - len(pivots), pivot_fn)
+
+
+print(quickselect_median(array))
