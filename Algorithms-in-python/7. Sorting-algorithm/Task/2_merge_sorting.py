a367a1d88e355eae61a85c906ecfc2c3c7530218
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-06 12:08:39 +0700

    Сортировка слиянием

--- Algorithms-in-python/7. Sorting-algorithm/Task/2_merge_sorting.py
+++ Algorithms-in-python/7. Sorting-algorithm/Task/2_merge_sorting.py
@@ -9,23 +9,23 @@ print(array)
 
 def merge(left_list, right_list):
     sorted_list = []
-    left_list_index = right_list_index = 0
-    left_list_length, right_list_length = len(left_list), len(right_list)
+    left_index = right_index = 0
+    left_length, right_length = len(left_list), len(right_list)
 
-    for _ in range(left_list_length + right_list_length):
-        if left_list_index < left_list_length and right_list_index < right_list_length:
-            if left_list[left_list_index] <= right_list[right_list_index]:
-                sorted_list.append(left_list[left_list_index])
-                left_list_index += 1
+    for _ in range(left_length + right_length):
+        if left_index < left_length and right_index < right_length:
+            if left_list[left_index] <= right_list[right_index]:
+                sorted_list.append(left_list[left_index])
+                left_index += 1
             else:
-                sorted_list.append(right_list[right_list_index])
-                right_list_index += 1
-        elif left_list_index == left_list_length:
-            sorted_list.append(right_list[right_list_index])
-            right_list_index += 1
-        elif right_list_index == right_list_length:
-            sorted_list.append(left_list[left_list_index])
-            left_list_index += 1
+                sorted_list.append(right_list[right_index])
+                right_index += 1
+        elif left_index == left_length:
+            sorted_list.append(right_list[right_index])
+            right_index += 1
+        elif right_index == right_length:
+            sorted_list.append(left_list[left_index])
+            left_index += 1
     return sorted_list
 
 
