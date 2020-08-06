e68232e47d5f569dce7ea6a5eb7c4ffa53651ca2
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-06 12:05:57 +0700

    Медиана

--- Algorithms-in-python/7. Sorting-algorithm/Task/3_median.py
+++ Algorithms-in-python/7. Sorting-algorithm/Task/3_median.py
@@ -6,12 +6,11 @@ random.shuffle(array)
 print(array)
 
 
-def quickselect_median(l, pivot_fn=random.choice):
-    if len(l) % 2 == 1:
-        return quickselect(l, len(l) / 2, pivot_fn)
+def median(array, pivot_fn=random.choice):
+    if len(array) % 2 == 1:
+        return quickselect(array, len(array) / 2, pivot_fn)
     else:
-        return 0.5 * (quickselect(l, len(l) / 2 - 1, pivot_fn) +
-                      quickselect(l, len(l) / 2, pivot_fn))
+        return 0.5 * (quickselect(array, len(array) / 2 - 1, pivot_fn) + quickselect(array, len(array) / 2, pivot_fn))
 
 
 def quickselect(l, k, pivot_fn):
@@ -21,9 +20,16 @@ def quickselect(l, k, pivot_fn):
 
     pivot = pivot_fn(l)
 
-    lows = [el for el in l if el < pivot]
-    highs = [el for el in l if el > pivot]
-    pivots = [el for el in l if el == pivot]
+    lows = []
+    highs = []
+    pivots = []
+    for i in l:
+        if i < pivot:
+            lows.append(i)
+        elif i > pivot:
+            highs.append(i)
+        else:
+            pivots.append(i)
 
     if k < len(lows):
         return quickselect(lows, k, pivot_fn)
@@ -33,4 +39,4 @@ def quickselect(l, k, pivot_fn):
         return quickselect(highs, k - len(lows) - len(pivots), pivot_fn)
 
 
-print(quickselect_median(array))
+print(median(array))
