be115f8f466c19606f8d19d5fdf8351ee2793a01
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-13 19:05:33 +0700

    Алгоритм Хаффмана

---
+++ Algorithms-in-python/9. Trees/Task_1/Task_2.py
@@ -0,0 +1,58 @@
+import heapq
+from collections import Counter, namedtuple
+
+
+class Node(namedtuple('Node', ['left', 'right'])):
+    def walk(self, code, acc):
+        self.left.walk(code, acc + '0')
+        self.right.walk(code, acc + '1')
+
+
+class Leaf(namedtuple('Leaf', ['char'])):
+    def walk(self, code, acc):
+        code[self.char] = acc or '0'
+
+
+def huffman_encode(s):
+    h = []
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+    heapq.heapify(h)
+    count = len(h)
+    while len(h) > 1:
+        freq1, _count1, left = heapq.heappop(h)
+        freq2, _count2, right = heapq.heappop(h)
+
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+    code = {}
+    if h:
+        [(_freq, _count, root)] = h
+        root.walk(code, "")
+    return code
+
+
+def huffman_decode(encoded, code):
+    sx = []
+    enc_ch = ""
+    for ch in encoded:
+        enc_ch += ch
+        for dec_ch in code:
+            if code.get(dec_ch) == enc_ch:
+                sx.append(dec_ch)
+                enc_ch = ''
+                break
+    return ''.join(sx)
+
+
+def huffman(s):
+    code = huffman_encode(s)
+    encoded = ''.join(code[ch] for ch in s)
+    print(len(code), len(encoded))
+    for ch in sorted(code):
+        print("{}: {}".format(ch, code[ch]))
+    print(encoded)
+    print(huffman_decode(encoded, code))
+
+
+huffman('beep boop beer!')
