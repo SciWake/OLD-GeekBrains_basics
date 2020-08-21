9e65b1860551153974b8df9e313c4d72c2a66675
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-21 14:50:13 +0700

    Изменены все первичные ключи на "id SERIAL PRIMARY KEY". Добавлена индексация.

--- Databases/2. SQL/shop.sql
+++ Databases/2. SQL/shop.sql
@@ -1,12 +1,13 @@
+-- SERIAL == BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE
 DROP TABLE IF EXISTS catalogs;
 CREATE TABLE catalogs (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Название раздела'
 ) COMMENT = 'Разделы интернет-магазина';
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Имя покупателя',
   brithday_at DATE COMMENT 'Дата рождения',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -15,26 +16,35 @@ CREATE TABLE users (
 
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Название',
   description TEXT COMMENT 'Описание',
   price DECIMAL(11, 2) COMMENT 'Цена',
   catalog_id INT UNSIGNED,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id(catalog_id)
 ) COMMENT = 'Товарные позиции';
+
+-- Добавление индекса в уже существующую таблицу:
+-- CREATE INDEX index_of_catalog_id USING BTREE ON products (catalog_id);
+-- USING BTREE - Указывает тип индекса (BTREE - Бинарное дерево, HASH - хэш-таблица)
+
+-- Удаление индекса из таблицы:
+-- DROP INDEX index_of_catalog_id ON products;
 
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   user_id INT UNSIGNED,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id)
 ) COMMENT = 'Заказы';
 
 DROP TABLE IF EXISTS orders_products;
 CREATE TABLE orders_products (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   order_id INT UNSIGNED,
   product_id INT UNSIGNED,
   total INT UNSIGNED DEFAULT 1 COMMENT 'Количество заказанных товарных позиций',
@@ -44,19 +54,21 @@ CREATE TABLE orders_products (
 
 DROP TABLE IF EXISTS discounts;
 CREATE TABLE discounts (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   useer_id INT UNSIGNED,
   product_id INT UNSIGNED,
   discount FLOAT UNSIGNED COMMENT 'Величина скидки от 0.0 до 1.0',
   started_at DATETIME,
   finished_at DATETIME,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_user_id(user_id),
+  KEY index_of_product_id(product_id)
 ) COMMENT = 'Скидки';
 
 DROP TABLE IF EXISTS storehouses;
 CREATE TABLE storehouses (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Название',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -64,7 +76,7 @@ CREATE TABLE storehouses (
 
 DROP TABLE IF EXISTS storehouses_products;
 CREATE TABLE storehouses_products (
-  id INT UNSIGNED NOT NULL,
+  id SERIAL PRIMARY KEY,
   storehouses_id INT UNSIGNED,
   product_id INT UNSIGNED,
   value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
