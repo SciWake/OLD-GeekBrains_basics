25032a9be0656508ad7285a2b2a46be105b21543
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-27 19:24:26 +0700

    Удалены излишки и ошибки

--- Databases/4. CRUD/correction_data.sql
+++ Databases/4. CRUD/correction_data.sql
@@ -19,22 +19,12 @@ UPDATE users SET updated_at = NOW() WHERE updated_at < created_at;
 SELECT * FROM users WHERE updated_at < created_at;
 
 
--- profiles
--- Смотрим структуру профилей
-DESC profiles;
-
--- Анализируем данные
-SELECT * FROM profiles LIMIT 10;
-
-SELECT * FROM media;
-
-
 -- messages
--- Смотрим структуру таблицы сообщений
-DESC messages;
-
 -- Анализируем данные
 SELECT * FROM messages LIMIT 10;
+
+-- Смотрим структуру таблицы сообщений
+DESC messages;
 
 -- Проверяем сколько сообщений отправлено самому себе
 SELECT COUNT(*) FROM messages WHERE from_user_id = to_user_id;
@@ -59,27 +49,57 @@ SELECT COUNT(*) FROM messages WHERE updated_at < created_at;
 
 
 -- media_types
--- Смотрим структуру таблицы медиаконтента 
-DESC media_types;
-
 -- Анализируем типы медиаконтента
 SELECT * FROM media_types;
+
+-- Смотрим структуру таблицы медиаконтента 
+DESC media_types;
 
 -- Заменяем стоблцы местами, где updated_at больше чем created_at
 INSERT INTO `media_types` SELECT * FROM `media_types` `t2` 
   WHERE `updated_at` < `created_at` 
     ON DUPLICATE KEY UPDATE `created_at` = `t2`.`updated_at`, `updated_at` = `t2`.`created_at`;
     
--- Исправим значения в таблицы name
--- Создаём временную таблицу
-CREATE TEMPORARY TABLE extensions (name VARCHAR(10));
+-- Исправим значения name в таблице
+UPDATE media_types SET name = 'photo' WHERE id = 1; 
+UPDATE media_types SET name = 'video' WHERE id = 2; 
+UPDATE media_types SET name = 'text' WHERE id = 3; 
+UPDATE media_types SET name = 'audio' WHERE id = 4; 
 
--- Добалвяем необходимые значения таблицу
-INSERT INTO extensions VALUES ('jpeg'), ('MP3'), ('MOV'), ('txt');
+-- Выполняем проверку
+SELECT * FROM media_types;
 
 
 -- media
+-- Анализируем данные
+SELECT * FROM media LIMIT 10;
+
+-- Переименуем столбец пути к файлу 
+ALTER TABLE media RENAME COLUMN filename TO filepath;
+
+-- Создаём временную таблицу форматов медиафайлов
+CREATE TEMPORARY TABLE extensions (name VARCHAR(10));
+
+-- Заполняем значениями
+INSERT INTO extensions VALUES ('jpeg'), ('MP3'), ('txt'), ('MP4');
+
+-- Проверяем
+SELECT * FROM extensions;
+
+-- Обновляем ссылку на файл
+UPDATE media SET filepath = CONCAT(
+  'https://',
+  filepath,
+  (SELECT last_name FROM users ORDER BY RAND() LIMIT 1),
+  '.',
+  (SELECT name FROM extensions ORDER BY RAND() LIMIT 1)
+);
+
+-- Проверяем замену
 SELECT * FROM media;
+
+-- Обновляем размер файлов
+UPDATE media SET size = FLOOR(10000 + (RAND() * 1000000)) WHERE size < 1000;
 
 -- Заполняем метаданные
 UPDATE media SET metadata = CONCAT('{"owner":"', 
@@ -89,5 +109,3 @@ UPDATE media SET metadata = CONCAT('{"owner":"',
 -- Возвращаем столбцу метеданных правильный тип
 ALTER TABLE media MODIFY COLUMN metadata JSON;
 
-
--- Видим ошибки в стобце metadata
