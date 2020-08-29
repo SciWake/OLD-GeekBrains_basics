f49c356432c861208da686f70d2639819ae39f63
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-29 22:44:38 +0700

    Домашняя работа 2 Выполнено задание 2

--- Databases/2. SQL/task_2.sql
+++ Databases/2. SQL/task_2.sql
@@ -47,3 +47,41 @@ SELECT * FROM catalogs;
 -- Содержание таблицы изменилось в 4 строке, значение NULL заменилось на empty, но 5 строка осталась NULL
 
 -- Вывод: Данное задание выполнить не получится, иначе будет нарушена целостность данных.
+
+/*
+2. Спроектируйте базу данных, которая позволяла бы организовать хранение медиафайлов, загружаемых пользователем (фото, аудио, видео). 
+Сами файлы будут храниться в файловой системе, а база данных будет содержать только пути к файлам, названия, 
+описание ключевых слов и принадлежности пользователю.
+*/
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя пользователя',
+  brithday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Пользователь';
+
+--  brithday_at DATE COMMENT 'Дата рождения', - данный столбец можно было опустить
+
+DROP TABLE IF EXISTS media;
+CREATE TABLE media (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пользователя, который загрузил файл",
+  filename VARCHAR(255) NOT NULL COMMENT "Путь к файлу",
+  size INT NOT NULL COMMENT "Размер файла",
+  metadata JSON COMMENT "Метаданные файла",
+  media_type_id INT UNSIGNED NOT NULL COMMENT "Ссылка на тип контента",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Медиафайлы";
+
+DROP TABLE IF EXISTS media_types;
+CREATE TABLE media_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  name VARCHAR(255) NOT NULL UNIQUE COMMENT "Название типа",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Типы медиафайлов";
+  
