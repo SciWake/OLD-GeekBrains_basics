5216644a78f421f9022d91a74bec57241e478625
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-30 22:00:06 +0700

    Домашняя работа

--- Databases/5. Data_aggregation/task_3.sql
+++ Databases/5. Data_aggregation/task_3.sql
@@ -104,3 +104,34 @@ SELECT * FROM storehouses_products;
 
 -- Сортируем значения
 SELECT * FROM storehouses_products ORDER BY IF(value > 0, 0, 1), value;
+
+
+/*
+Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае. 
+Месяцы заданы в виде списка английских названий (may, august)
+*/
+
+-- Cоздаём таблицу пользователй
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+-- Заполняем таблицу данными
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+
+-- Проверяем данные
+SELECT * FROM users;
+
+-- Выполянем запрос
+SELECT * FROM users WHERE DATE_FORMAT(birthday_at, '%M') IN ('August', 'May');
