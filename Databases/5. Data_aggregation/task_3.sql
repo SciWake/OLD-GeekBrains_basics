256699064f1d7a16164c576a0de1df4c73e9b48f
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-30 21:26:24 +0700

    Домашняя работа 3

--- Databases/5. Data_aggregation/task_3.sql
+++ Databases/5. Data_aggregation/task_3.sql
@@ -77,3 +77,30 @@ SELECT * FROM users;
 
 -- Проверим структуру
 DESC users;
+
+
+/*
+В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры: 0, 
+если товар закончился и выше нуля, если на складе имеются запасы. Необходимо отсортировать записи таким образом, 
+чтобы они выводились в порядке увеличения значения value. Однако нулевые запасы должны выводиться в конце, после всех записей.
+*/
+
+-- Создаём таблицу запасов на складе
+DROP TABLE IF EXISTS storehouses_products;
+CREATE TABLE storehouses_products (
+  id SERIAL PRIMARY KEY,
+  storehouse_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Запасы на складе';
+
+-- Заполняем таблицу данными
+INSERT INTO storehouses_products (value) VALUES (0), (2500), (0), (30), (500), (1);
+
+-- Проверяем данные
+SELECT * FROM storehouses_products;
+
+-- Сортируем значения
+SELECT * FROM storehouses_products ORDER BY IF(value > 0, 0, 1), value;
