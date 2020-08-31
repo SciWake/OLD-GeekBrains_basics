a43dbbffcbe6aa3cdc6739a74cbc910475ed25d8
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-08-31 14:02:57 +0700

    Домашняя работа 4

---
+++ Databases/5. Data_aggregation/task_4.sql
@@ -0,0 +1,33 @@
+/* Подсчитайте средний возраст пользователей в таблице users. */
+
+-- Используем таблицу shop
+USE shop;
+
+-- Создаём таблицу пользователей
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+-- Заполняем таблицу данными
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+
+-- Подсчитываем средний возраст всех пользователей  
+SELECT AVG((TO_DAYS(NOW()) - TO_DAYS(birthday_at)) / 365.25) AS mean_age FROM users;
+-- 30.7
+
+
+/*
+Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. 
+Следует учесть, что необходимы дни недели текущего года, а не года рождения.
+*/
