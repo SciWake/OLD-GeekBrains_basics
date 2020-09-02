9808c071d3cc64fe37ee71dce3bbd5dd0f145a45
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-02 23:38:56 +0700

    Генерация данных для таблицы лайков

--- Databases/6. Data_aggregation_conference/refinement_structure.sql
+++ Databases/6. Data_aggregation_conference/refinement_structure.sql
@@ -1,5 +1,7 @@
 -- Дополнение по структуре БД vk
 
+USE vk;
+
 -- Таблица лайков
 DROP TABLE IF EXISTS likes;
 CREATE TABLE likes (
@@ -17,3 +19,25 @@ CREATE TABLE target_types (
   name VARCHAR(255) NOT NULL UNIQUE,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+
+-- Заполнение данных
+-- Добавление типов, что можно лайкать
+INSERT INTO target_types (name) VALUES 
+  ('messages'),
+  ('users'),
+  ('media'),
+  ('posts');
+
+-- Заполняем лайки
+INSERT INTO likes 
+  SELECT 
+    id, 
+    FLOOR(1 + (RAND() * 300)), 
+    FLOOR(1 + (RAND() * 300)),
+    FLOOR(1 + (RAND() * 4)),
+    CURRENT_TIMESTAMP 
+  FROM messages;
+  
+-- Проверка данных
+SELECT * FROM likes;
