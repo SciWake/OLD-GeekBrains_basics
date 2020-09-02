bb630427f5b0edb282fb03839205ae1cd5a0e0e4
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-02 23:06:45 +0700

    Создана таблица лайков

---
+++ Databases/6. Data_aggregation_conference/refinement_structure.sql
@@ -0,0 +1,19 @@
+-- Дополнение по структуре БД vk
+
+-- Таблица лайков
+DROP TABLE IF EXISTS likes;
+CREATE TABLE likes (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  target_id INT UNSIGNED NOT NULL,
+  target_type_id INT UNSIGNED NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Таблица типов лайков
+DROP TABLE IF EXISTS target_types;
+CREATE TABLE target_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
