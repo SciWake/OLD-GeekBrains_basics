751c2626f89f890781ab5057e0d2c5292aa5660e
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-04 00:07:11 +0700

    Исправлена структура внешнего ключа в таблице communities_users

--- Databases/6. Data_aggregation_conference/foreign_key.sql
+++ Databases/6. Data_aggregation_conference/foreign_key.sql
@@ -135,7 +135,6 @@ DESC communities_users;
 -- Добавляем внешние ключи
 ALTER TABLE communities_users
   ADD CONSTRAINT communities_users_community_id_fk
-    FOREIGN KEY (community_id) REFERENCES communities(id)
-      ON DELETE CASCADE,
+    FOREIGN KEY (community_id) REFERENCES communities(id),
   ADD CONSTRAINT communities_users_user_id_fk
     FOREIGN KEY (user_id) REFERENCES users(id);
