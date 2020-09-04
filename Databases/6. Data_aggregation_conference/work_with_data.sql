d8f54780463bbcfa609a5dab44e13b716be6c69b
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-04 19:39:09 +0700

    Поиск пользователя по шаблонам имени

--- Databases/6. Data_aggregation_conference/work_with_data.sql
+++ Databases/6. Data_aggregation_conference/work_with_data.sql
@@ -147,3 +147,8 @@ SELECT from_user_id,
 SELECT CONCAT(first_name, ' ', last_name) AS fullname  
   FROM users
   WHERE first_name LIKE 'M%';
+  
+-- Используем регулярные выражения
+SELECT CONCAT(first_name, ' ', last_name) AS fullname  
+  FROM users
+  WHERE last_name RLIKE '^K.*r$';
