c371d48adb9b3f4b068558fcf77d6660ada31d9a
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-04 19:12:08 +0700

    Выбираем историю по добавлению фотографий пользователем

--- Databases/6. Data_aggregation_conference/work_with_data.sql
+++ Databases/6. Data_aggregation_conference/work_with_data.sql
@@ -38,3 +38,16 @@ SELECT filepath FROM media
     AND media_type_id = (
       SELECT id FROM media_types WHERE name = 'photo'
     );
+
+
+-- Выбираем историю по добавлению фотографий пользователем
+SELECT CONCAT(
+  'Пользователь ', 
+  (SELECT CONCAT(first_name, ' ', last_name) FROM users WHERE id = media.user_id),
+  ' добавил фото ', 
+  filepath, ' ', 
+  created_at) AS news 
+    FROM media 
+    WHERE user_id = 8 AND media_type_id = (
+        SELECT id FROM media_types WHERE name LIKE 'photo'
+);
