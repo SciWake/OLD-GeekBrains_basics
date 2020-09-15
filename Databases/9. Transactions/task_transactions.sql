ccfe5929389503b8b2ecb8919b99bf10dc3a78e7
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-15 23:48:45 +0700

    Добавлена возможность удаления записи из таблицы при совершении транзакции.

--- Databases/9. Transactions/task_transactions.sql
+++ Databases/9. Transactions/task_transactions.sql
@@ -1,7 +1,7 @@
 /* В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции. */
 
 --  Создание структуры и заполнение таблиц в БД sample
-USE sample;
+USE shop;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
@@ -22,7 +22,7 @@ INSERT INTO users (name, birthday_at) VALUES
 
 
 --  Создание структуры и заполнение таблиц в БД shop
-USE shop;
+USE sample;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
@@ -33,22 +33,76 @@ CREATE TABLE users (
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) COMMENT = 'Покупатели';
 
-INSERT INTO users (name, birthday_at) VALUES
-  ('Виталий', '2001-10-05'),
-  ('Андрей', '2012-11-12'),
-  ('Алексей', '1999-05-20'),
-  ('Елена', '2008-02-14'),
-  ('Валентин', '2006-01-12'),
-  ('Мария', '2014-08-29');
 
 START TRANSACTION;
 
-UPDATE sample.users SET name = (SELECT name FROM shop.users WHERE id = 1) WHERE id = 1;
-UPDATE sample.users SET birthday_at = (SELECT birthday_at FROM shop.users WHERE id = 1) WHERE id = 1;
-UPDATE sample.users SET created_at = (SELECT created_at FROM shop.users WHERE id = 1) WHERE id = 1;
-UPDATE sample.users SET updated_at = (SELECT updated_at FROM shop.users WHERE id = 1) WHERE id = 1;
+INSERT INTO sample.users SELECT * FROM shop.users WHERE id = 1;
+DELETE FROM shop.users WHERE id = 1;
 
 COMMIT;
 
 -- Проверка работы транзакции
 SELECT * FROM sample.users;
+SELECT * FROM shop.users;
+
+
+
+/* Создайте представление, которое выводит название name товарной позиции из таблицы products и соответствующее название каталога name из таблицы catalogs. */
+
+-- Создаём необходимую структуру и заполняем её данным в БД shop
+USE shop;
+
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры'),
+  (NULL, 'Материнские платы'),
+  (NULL, 'Видеокарты'),
+  (NULL, 'Жесткие диски'),
+  (NULL, 'Оперативная память');
+
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  description TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+  
+
+-- Создаём запрос для проверки
+SELECT products.name, catalogs.name 
+  FROM products
+    LEFT JOIN catalogs
+      ON products.catalog_id = catalogs.id;
+
+-- Преобразуем запрос в представление
+CREATE VIEW product_name (name, type)
+  AS SELECT products.name, catalogs.name 
+	  FROM products
+		LEFT JOIN catalogs
+		  ON products.catalog_id = catalogs.id;
+
+-- Обращаемся к представлению
+SELECT * FROM product_name;
