3bc367e5afd2cce3d05ae70974aa0e16158eee2b
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-20 21:35:17 +0700

    Индексы

--- Databases/10. Transactions_conference/procedures_vk.sql
+++ Databases/10. Transactions_conference/procedures_vk.sql
@@ -1,5 +1,6 @@
 USE vk;
 
+-- ТРИГГЕРЫ
 
 /* Создание триггера для обработки target_id. Тригер будет работать аналогично тому, как СУБД проверяет допустимость значений внешнего ключа, 
 например для столбца user_id и ссылку на id из таблицы users.
@@ -90,3 +91,75 @@ INSERT INTO likes (user_id, target_id, target_type_id) VALUES (34, 56, 8);
 -- Просмотр функций и процедур
 SHOW FUNCTION STATUS LIKE 'is_row_exists';
 SHOW CREATE FUNCTION is_row_exists;
+
+
+
+-- ИНДЕКСЫ
+SELECT * FROM users;
+
+-- Рассмотрим запрос
+SELECT id, first_name, last_name 
+  FROM users 
+    WHERE email = 'Mireya12@example.com';
+
+CREATE INDEX users_email_idx ON users(email);
+
+-- Внутренности хранения индексов
+-- Представим, что наша таблица выглядит так:
+SELECT id, first_name, last_name, email FROM users;
+
+/* После создания индекса на колонку email, MySQL сохранит все ее значения в 
+отсортированном виде:
+users_email_idx
++-----------------------------+
+| acarroll@example.net        |
+| alvera.terry@example.org    |
+| alyce76@example.com         |
+| arianna46@example.net       |
+| arielle.murazik@example.org |
+| aurelio.abbott@example.org  |
+| beatty.tommie@example.com   |
+| bergnaum.asia@example.org   |
+| bergnaum.donato@example.org |
+| blaise68@example.org        |
++-----------------------------+
+
+После этой операции MySQL начнет использовать индекс users_email_idx для выполнения подобных запросов */
+
+-- Сортировка
+SELECT * FROM profiles ORDER BY birthday;
+
+-- действует такое же правило — создаем индекс на колонку, по которой происходит сортировка:
+CREATE INDEX profiles_birthday_idx ON profiles(birthday);
+
+-- MySQL поддерживает также уникальные индексы. Это удобно для колонок, 
+-- значения в которых должны быть уникальными по всей таблице. 
+
+-- Такие индексы улучшают эффективность выборки для уникальных значений. 
+SELECT * FROM users WHERE email = 'alexis29@example.org';
+
+-- На колонку email необходимо создать уникальный индекс:
+DROP INDEX users_email_idx ON users;
+CREATE UNIQUE INDEX users_email_uq ON users(email);
+-- СУБД будет знать, что такое значение одно и больше искать не требуется.
+
+-- Составные индексы
+-- Рассмотрим такой запрос:
+SELECT * FROM messages WHERE from_user_id = 9 AND to_user_id = 3;
+
+-- Нам следует создать составной индекс на обе колонки:
+CREATE INDEX messages_from_user_id_to_user_id_idx ON messages (from_user_id, to_user_id);
+
+-- Устройство составного индекса
+-- messages_from_user_id_to_user_id_idx
+-- 1 3
+-- 2 4
+-- 3 8
+-- ...
+
+-- Сортировка
+-- Составные индексы также можно использовать, если выполняется сортировка:
+SELECT * FROM profiles WHERE country = 'Russia' ORDER BY birthday;
+
+-- В этом случае нам нужно будет создать индекс в порядке
+-- WHERE ORDER BY
