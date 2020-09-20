875ea1000baf6d80a5aaa6e98a429a77da5d253f
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-20 18:30:59 +0700

    Создание триггера
    Тригер работает на вставку строки в таблицу Likes, который првоеряет по введённым значениям target_id и target_type_id, существует ли
    в соответсвующей таблице запись с представленным id. Если в соответствующей таблице, которая задана ссылкой в target_type_id нет идентификатора,
    который задан  в target_id, то будет генерироваться ошибка. Ошибка сообщает о том, что не ссылочной целостности (Попытка вставки данных, которых
    нет в связанных на ЛОГИЧЕСКОМ уровне таблицах).

--- Databases/10. Transactions_conference/procedures_vk.sql
+++ Databases/10. Transactions_conference/procedures_vk.sql
@@ -10,7 +10,7 @@ USE vk;
 нет в связанных на ЛОГИЧЕСКОМ уровне таблицах).
 
 Сначала создадим функцию для проверки существования строки. Причиной этого служит, что тригеры проверяют данные по очень простым критериям,
-поэтому в тригерах не работают некоторые управляющие структуры, которые мы можем использовать в функциях или процедурах. Вся логика проверки 
+поэтому в триггерах не работают некоторые управляющие структуры, которые мы можем использовать в функциях или процедурах. Вся логика проверки 
 будет вынесена в функцию.
 */
 
@@ -59,3 +59,34 @@ SELECT is_row_exists(300, 2);
 SELECT is_row_exists(301, 2);
 -- 0
 -- Мы знаем, что в таблице пользователей, их всего 300
+
+
+-- Создадим триггер для проверки валидности target_id и target_type_id
+ 
+DROP TRIGGER IF EXISTS like_validation;
+DELIMITER //
+
+CREATE TRIGGER like_validation BEFORE INSERT ON likes
+
+FOR EACH ROW BEGIN
+  IF NOT is_row_exists(NEW.target_id, NEW.target_type_id) THEN
+    SIGNAL SQLSTATE "45000"
+    SET MESSAGE_TEXT = "Error adding like! Target table doesn't contain row id provided!";
+  END IF;
+  
+END//
+
+DELIMITER ;
+
+-- Проверка работы триггера 
+INSERT INTO likes (user_id, target_id, target_type_id) VALUES (34, 56, 2);
+-- Вставка прошла успешно
+
+-- Изменим target_type_id на другое значение
+INSERT INTO likes (user_id, target_id, target_type_id) VALUES (34, 56, 8);
+-- Такого id в таблице target_types нет, поэтому триггер сообщает нам об ошибке
+
+
+-- Просмотр функций и процедур
+SHOW FUNCTION STATUS LIKE 'is_row_exists';
+SHOW CREATE FUNCTION is_row_exists;
