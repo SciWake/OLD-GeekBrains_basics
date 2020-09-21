1fce8621a3a47efadc3d6249dd16daa22a96686d
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-21 17:02:22 +0700

    Создание оконной функции
    Данный запрос выводит информацию:
    имя группы
    среднее количество пользователей в группах
    самый молодой пользователь в группе
    самый старший пользователь в группе
    общее количество пользователей в группе
    всего пользователей в системе
    отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100 */

--- Databases/10. Transactions_conference/procedures_vk.sql
+++ Databases/10. Transactions_conference/procedures_vk.sql
@@ -242,3 +242,32 @@ SELECT user_id, city, birthday,
   NTH_VALUE(city, 2) OVER w AS 'second'
     FROM profiles
       WINDOW w AS (PARTITION BY LEFT(birthday, 3) ORDER BY birthday);
+
+
+-- ЗАДНИЯ
+
+/* Задание на оконные функции
+Построить запрос, который будет выводить следующие столбцы:
+имя группы
+среднее количество пользователей в группах
+самый молодой пользователь в группе
+самый старший пользователь в группе
+общее количество пользователей в группе
+всего пользователей в системе
+отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100 */
+
+SELECT DISTINCT 
+  communities.name AS group_name,
+  COUNT(communities_users.user_id) OVER() / (SELECT COUNT(*) FROM communities) AS avg_users_in_groups,
+  FIRST_VALUE(users.id) OVER(PARTITION BY communities.id ORDER BY profiles.birthday DESC) AS youngest_user,
+  FIRST_VALUE(users.id) OVER(PARTITION BY communities.id ORDER BY profiles.birthday) AS oldest,
+  COUNT(communities_users.user_id) OVER(PARTITION BY communities.id) AS users_in_groups,
+  COUNT(communities_users.user_id) OVER() AS users_total,
+  COUNT(communities_users.user_id) OVER(PARTITION BY communities.id) / COUNT(communities_users.user_id) OVER() *100 AS '%%'
+    FROM communities
+      LEFT JOIN communities_users 
+        ON communities_users.community_id = communities.id
+      JOIN users 
+        ON communities_users.user_id = users.id
+      JOIN profiles 
+        ON profiles.user_id = users.id;
