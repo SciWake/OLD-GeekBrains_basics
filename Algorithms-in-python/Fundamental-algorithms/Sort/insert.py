89b04203cf3189fb22519a1e454e30faa18b2087
Aleksey Kuritsyn <bimastics@yandex.com>
2021-04-01 21:01:28 +0700

    Сортировка вставками

--- Algorithms-in-python/Fundamental-algorithms/Sort/insert.py
+++ Algorithms-in-python/Fundamental-algorithms/Sort/insert.py
@@ -24,13 +24,13 @@ random.shuffle(mass)
 def insertion_sort(array):
     for i in range(1, len(array)):
         item_to_insert = array[i]
-        j = i
+        j = i - 1
 
-        while array[j - 1] > item_to_insert and j > 0:
-            array[j] = array[j - 1]
+        while j >= 0 and array[j] > item_to_insert:
+            array[j + 1] = array[j]
             j -= 1
-
-        array[j] = item_to_insert
+        # Вставляем элемент
+        array[j + 1] = item_to_insert
 
 # python -m timeit -n 1000 "import insert" "insert.insertion_sort(insert.mass)"
 # 1000 loops, best of 5: 4.45 usec per loop
